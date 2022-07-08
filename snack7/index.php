84769c3309d44299e4de191863f0a8f2c31461b3
MyUsername <vanaliedoardo@gmail.com>
2022-07-08 18:14:42 +0200

    snack7 da finire

---
+++ snack7/index.php
@@ -0,0 +1,45 @@
+<?php
+
+/*Creare un array contenente qualche alunno di un’ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Stampare Nome, Cognome e la media dei voti di ogni alunno.*/
+
+$listaAlunni = [
+    [
+        "nome" => "Giovanni",
+        "cognome" => "Bianchi",
+        "voti" => [5, 7, 8, 9, 10],
+    ],
+    [
+        "nome" => "Roberto",
+        "cognome" => "Rossi",
+        "voti" => [10, 9, 8, 7, 6],
+    ],
+    [
+        "nome" => "Mario",
+        "cognome" => "Verdi",
+        "voti" => [6, 7, 8, 9, 10],
+    ],
+    [
+        "nome" => "Paolo",
+        "cognome" => "Bianchi",
+        "voti" => [5, 7, 8, 9, 10],
+    ],
+    [
+        "nome" => "Giacomo",
+        "cognome" => "Neri",
+        "voti" => [10, 9, 8, 7, 6],
+    ],
+    [
+        "nome" => "Roberto",
+        "cognome" => "Bianchi",
+        "voti" => [5, 7, 8, 9, 10],
+    ],
+
+    [
+        "nome" => "Roberto",
+        "cognome" => "Rossi",
+        "voti" => [10, 9, 8, 7, 6],
+    ],
+
+];
